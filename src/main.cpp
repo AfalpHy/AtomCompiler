f21b95d3fb7d67da937eb834cf538b116a6f214f
Thirty-creed <736353503@qq.com>
2023-11-07 23:09:47 +0800

    build the ctest

--- src/main.cpp
+++ src/main.cpp
@@ -17,11 +17,49 @@ using namespace std;
 using namespace antlr4;
 
 int main(int argc, const char *argv[]) {
+    int current = 1;
+    string sourceFile;
+    string otherSrcFile;
+    string runtimeInput;
+    string compareFile;
+    string compareResult;
+    bool useLLVM = false;
+    bool runAfterCompile = false;
+    bool dumpAst = false;
+    bool check = false;
+
+    while (current < argc) {
+        if (strcmp(argv[current], "--emit-llvm") == 0) {
+            useLLVM = true;
+        } else if (strcmp(argv[current], "--other-src") == 0) {
+            current++;
+            otherSrcFile = argv[current];
+        } else if (strcmp(argv[current], "-R") == 0) {
+            runAfterCompile = true;
+        } else if (strcmp(argv[current], "-R-in") == 0) {
+            current++;
+            runtimeInput = argv[current];
+        } else if (strcmp(argv[current], "--dump-ast") == 0) {
+            dumpAst = true;
+        } else if (strcmp(argv[current], "--check") == 0) {
+            check = true;
+        } else if (strcmp(argv[current], "--compare-file") == 0) {
+            current++;
+            compareFile = argv[current];
+        } else if (strcmp(argv[current], "--compare-result") == 0) {
+            current++;
+            compareResult = argv[current];
+        } else {
+            sourceFile = argv[current];
+        }
+        current++;
+    }
+
     std::ifstream file;
-    file.open(argv[1]);
+    file.open(sourceFile);
 
     if (!file.is_open()) {
-        cerr << filesystem::absolute(argv[1]) << " not exist" << endl;
+        cerr << filesystem::absolute(sourceFile) << " not exist" << endl;
         return 0;
     }
     ANTLRInputStream input(file);
@@ -43,17 +81,44 @@ int main(int argc, const char *argv[]) {
     //     compUnit->accept(&checker);
     // }
 
-    if (argc > 2 && strcmp(argv[2], "--dump-ast") == 0) {
+    if (dumpAst) {
         ATC::DumpASTVisitor dump;
         for (auto compUnit : ATC::CompUnit::AllCompUnits) {
             compUnit->accept(&dump);
         }
     }
 
-    ATC::IRBuilder irBuilder;
-    for (auto compUnit : ATC::CompUnit::AllCompUnits) {
-        compUnit->accept(&irBuilder);
+    if (useLLVM) {
+        ATC::IRBuilder irBuilder;
+        for (auto compUnit : ATC::CompUnit::AllCompUnits) {
+            compUnit->accept(&irBuilder);
+        }
+        std::filesystem::path filePath = sourceFile;
+        string filename = filePath.stem();
+        string llFile = filename + ".ll";
+        string outFile = filename + ".out";
+        irBuilder.dumpLL(llFile);
+        string cmd = "clang";
+        cmd.append(" ").append(llFile).append(" ").append(otherSrcFile);
+        system(cmd.c_str());
+        if (runAfterCompile) {
+            cmd = "./a.out";
+            if (!runtimeInput.empty()) {
+                cmd.append(" < ").append(runtimeInput);
+            }
+            if (check) {
+                cmd.append(" > ").append(outFile);
+                int ret = system(cmd.c_str());
+                ret = WEXITSTATUS(ret);
+                cmd = "echo";
+                cmd.append(" ").append(to_string(ret)).append(" >> ").append(outFile);
+                system(cmd.c_str());
+                cmd = "diff";
+                cmd.append(" ").append(outFile).append(" ").append(compareFile).append(" >> ").append(compareResult);
+                system(cmd.c_str());
+            }
+        }
     }
-    irBuilder.dumpLL("main.ll");
+
     return 0;
 }
